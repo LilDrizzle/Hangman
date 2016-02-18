a4a735e97744012a4ce52c3615fc0dedb720aae6
LilDrizzle <maxbroni1@gmail.com>
2016-02-18 09:42:43 -0600

    Update hangman.java

--- hangman.java
+++ hangman.java
@@ -6,7 +6,7 @@ import java.io.*;
 
 // WARNING: IF THIS COMMENT IS DELETED THE PROGRAM WONT COMPILE
 
-// Current problems: 	amountWrong is always 0		word never equals originalString		haven't tested hangTheMan() yet
+// Current problems: 	haven't tested hangTheMan() yet
 
 public class Hangman {
 	public static void main(String[] args) throws IOException {			//wanna make it so it does try catch instead
@@ -24,11 +24,11 @@ public class Hangman {
 		Random generator = new Random();
 		StringBuilder word = new StringBuilder();
 		Scanner inputFile = new Scanner(wordListFile);
-		String originalString = "";	
+		
 		char guess;
 		int wordIndex = generator.nextInt(20000);
 		StringBuilder hangman = new StringBuilder(" ---------" + 
-												  "\n |       |" + 
+												  "\n  |      |" + 
 												  "\n         |" +														// 23 and 25
 												  "\n         |" +														// 34 and 36
 												  "\n         |" +															// 46
@@ -42,7 +42,7 @@ public class Hangman {
 		for (int c = 0; c < wordIndex - 1; c++) {					// what if wordIndex = 0 or 1?
 			inputFile.nextLine();				
 		}
-		originalString += inputFile.nextLine();
+		StringBuilder originalString = new StringBuilder(inputFile.nextLine());
 		
 		// Creates the string with _ for letters
 		for (int c = 0; c < originalString.length(); c++)  {
@@ -56,7 +56,7 @@ public class Hangman {
 			//System.out.println("Congrats you guessed the word after " + tries + " tries.");
 	}		
 	
-	public static void test(char guess, String originalString, StringBuilder word, Scanner keyboard, StringBuilder hangman) {
+	public static void test(char guess, StringBuilder originalString, StringBuilder word, Scanner keyboard, StringBuilder hangman) {
 		
 		boolean correct = false;
 		int amountWrong = 0;
@@ -66,14 +66,15 @@ public class Hangman {
 			
 			// Checks if your guess is equal to a letter in the word
 			for (int c = 0; c < originalString.length(); c++) {		
-				if (guess == originalString.charAt(c)) 					
+				if (guess == originalString.charAt(c)) {					
 					word.setCharAt(c, guess);
 					correct = true;
+				}
 			}
 			
 			// Builds the body of the hangman
 			if (correct == false) {
-				amountWrong += 1;
+				amountWrong++;
 				hangTheMan(amountWrong, hangman);
 			}
 			
@@ -82,12 +83,8 @@ public class Hangman {
 			System.out.println("\n" + word + "\n");
 			correct = false;
 			guess = keyboard.next().charAt(0);
-			
-			//if (word.equals(originalString)) 
-				//	System.out.println("BLASJFGASJKGFIJUASFJSAAKJ");
-			
-			//System.out.println(amountWrong);
-		} while(!(word.equals(originalString)));
+
+		} while(!(word.toString().equals(originalString.toString())));		// have to use the toString() the normal StringBuilders didn't work
 	}
 	
 	public static void hangTheMan(int amountWrong, StringBuilder hangman) {
@@ -95,29 +92,40 @@ public class Hangman {
 		switch (amountWrong) {
 			case 1: 
 				hangman.setCharAt(23, '/');
+				break;
 			case 2:
 				hangman.setCharAt(25, '\\');
+				break;
 			case 3:
 				hangman.setCharAt(34, '\\');
+				break;
 			case 4:
 				hangman.setCharAt(36, '/');
+				break;
 			case 5:
 				hangman.setCharAt(46, '|');
+				break;
 			case 6:
-				hangman.setCharAt(56, '|');
+				hangman.setCharAt(57, '|');
+				break;
 			case 7:
-				hangman.setCharAt(57, '\\');
+				hangman.setCharAt(56, '\\');
+				break;
 			case 8:
 				hangman.setCharAt(58, '/');
+				break;
 			case 9:
-				hangman.setCharAt(69, '|');
+				hangman.setCharAt(68, '|');
+				break;
 			case 10:
-				hangman.setCharAt(79, '/');
-			case 11:
-				hangman.setCharAt(81, '\\');
+				hangman.setCharAt(78, '/');
+				break;
+			case 11: 
+				hangman.setCharAt(80, '\\');
 				System.out.println(hangman);
 				System.out.println("You Lost.");
 				System.exit(0);
+			
 		}
 				
 	}
@@ -135,4 +143,4 @@ public class Hangman {
                 ________|_
 	
 					10x9
-*/							
+*/			
